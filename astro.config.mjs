952b60620cae178e7697a580adfd5b222dd06442
David Román <droman@omegacrmconsulting.com>
2024-07-08 20:07:01 +0200

    Add CI with Github Actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
 
+    const LIVE_URL = 'd4bit.github.io';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: LIVE_URL,
+});
